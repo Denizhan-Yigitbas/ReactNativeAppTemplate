6e03e60ee34157ac9b1c762bc8c64acb0bba4b43
Denizhan Yigitbas <denizhanyigitbas@gmail.com>
2020-07-11 17:44:32 -0600

    remove unneeded code

--- src/screens/MessagingScreen.js
+++ src/screens/MessagingScreen.js
@@ -1,11 +1,6 @@
 import React, { memo } from "react";
 import Background from "../components/Background";
-import Logo from "../components/Logo";
 import Header from "../components/Header";
-import Paragraph from "../components/Paragraph";
-import Button from "../components/Button";
-import { logoutUser } from "../api/auth-api";
-import {createBottomTabNavigator} from 'react-navigation-tabs';
 
 const MessagingScreen = () => (
   <Background>
